988fa6d6f04e09d98d945f90973725f3fc238f68
Guilherme-Rigobello <guilhermerigobello8@gmail.com>
2024-08-16 22:29:30 -0300

    mobile header style - script/css

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,42 @@
 const header = document.querySelector("header");
+const openMenu = document.querySelector(".menu-burger")
+const closeMenu = document.querySelector(".close-menu")
+
+// menu mobile script //
+
+openMenu.addEventListener('click', () => {
+    if (!openMenu) {
+        openMenu.style.display = 'flex';
+        closeMenu.style.display = 'none';
+    } else {
+        closeMenu.style.display = 'flex';
+        openMenu.style.display = 'none'
+    }
+})
+
+closeMenu.addEventListener('click', () => {
+    if (!closeMenu) {
+        openMenu.style.display = 'none';
+        closeMenu.style.display = 'flex';
+    } else {
+        closeMenu.style.display = 'none';
+        openMenu.style.display = 'flex'
+    }
+})
+
+// header script //
+
 let lastScroll = 0;
-
-
 window.addEventListener("scroll", () => {
     const currentScroll = window.scrollY;
-    
+
     if (currentScroll > lastScroll) {
         header.style.transform = "translateY(-100%)";
-    } else { 
+    } else {
         header.style.transform = "translateY(0%)";
     }
 
     lastScroll = currentScroll;
 });
+
 
--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require('express')
  
 const app = express()
  
-const port = 3000
+const port = 3002
 
 const plantasPath = path.join(__dirname, './assets/json/plantas.json')
 const plantasData = fs.readFileSync(plantasPath, 'utf-8')

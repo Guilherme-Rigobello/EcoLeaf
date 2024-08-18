2621e4e73390d35530806dc9c4d6bf4f5a006a56
Guilherme-Rigobello <guilhermerigobello8@gmail.com>
2024-08-17 23:09:47 -0300

    table and search output

--- server.js
+++ server.js
@@ -2,8 +2,9 @@ const fs = require('fs')
 const path = require('path')
 const express = require('express')
 const app = express()
- 
-const port = 3002
+
+//PORT
+const port = 4000
 
 const plantasPath = path.join(__dirname, './assets/json/plantas.json')
 const plantasData = fs.readFileSync(plantasPath, 'utf-8')
@@ -30,36 +31,39 @@ app.get('/', (req, res) => {
     const finalHtml = htmlContent.replace('{{plantasTable}}', plantasTable);
 
     res.send(finalHtml);
-})
+});
 
+app.get('/buscarPorNome', (req, res) => {
+    const nomePopular = req.query.nome.toLowerCase();
+    const nomesFiltrados = plantas.filter(planta => planta.nomePopular.toLowerCase() === nomePopular);
 
+    let plantasTable = '';
 
+    nomesFiltrados.forEach(planta => {
 
-function buscarPlanta(nomePopular) {
-    return plantas.find(planta => planta.nomePopular.toLowerCase() === nomePopular.toLowerCase())
-}
+        plantasTable += `
+   <tr >
+        <td style="padding-top: 35px;">${planta.id}</td>
+        <td style="padding-top: 35px;">${planta.nomeCientifico} </td>
+        <td style="padding-top: 35px;">${planta.nomePopular} </td>
+        <td><img src="${planta.foto}" alt="${planta.nomeCientifico}" style ="width: 200px; height: 80px;"></td>
+    </tr>
+    `
+});
 
-
-app.get('/buscar-planta', (req, res) => {
-    const nomeDaPlantaBuscada = req.query.nomePopular
-    const plantaEncontrada = buscarPlanta(nomeDaPlantaBuscada)
-
-    if (plantaEncontrada) {
-        const templatePath = path.join(__dirname, 'dadosplanta.html')
-        const templateData = fs.readFileSync(templatePath, 'utf-8')
-
-        const html = templateData
-            .replace('{{nomeCientifico}}', plantaEncontrada.nomeCientifico)
-            .replace('{{nomePopular}}', plantaEncontrada.nomePopular)
-            .replace('{{desc}}', plantaEncontrada.desc)
-            .replace('{{venenosa}}', plantaEncontrada.venenosa)
-            .replace('{{foto}}', plantaEncontrada.foto)
-
-        res.send(html)
-    } else {
-        res.send('<h1>planta não encontrada.</h1>')
-    }
-})
+const voltarLink = `
+        <tr>
+            <td colspan="4" style="text-align: center; padding-top: 10px; border:0 ">
+                <a class="btn btn-success rounded" href="/">Voltar</a>
+            </td>
+        </tr>
+    `;
+    
+    const htmlContent = fs.readFileSync('index.html', 'utf-8');
+    const finalHtml = htmlContent
+        .replace('{{plantasTable}}', plantasTable + voltarLink);
+    res.send(finalHtml);
+});
 
 
 app.listen(port, () => {
